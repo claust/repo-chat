7965afb69675b210a4ce9cdfca7d85680385d8ec
Claus Thomasen <clth@ski.dk>
2024-11-23 23:47:39 +0100

    Read files

--- packages/server/updater.ts
+++ packages/server/updater.ts
@@ -1,10 +1,24 @@
 import type { Context } from "hono";
+import { readdir } from "node:fs/promises";
 import type { BlankEnv, BlankInput } from "hono/types";
 
 export async function updater(c: Context<BlankEnv, "/updater", BlankInput>): Promise<void | Response> {
-    const fileName = './app.ts';
+    const includeText = c.req.query('includeText');
+    const files = await readdir('./', { recursive: true });
+    let result = '';
+    for (const file of files) {
+        const text = await handleFile(file);
+        result += `File: ${file}\n\n`;
+        if (includeText === 'true') {
+            result += `${text}\n\n`;
+        }
+    }
+    return c.text(result);
+}
+
+async function handleFile(fileName: string) {
     const file = Bun.file(fileName);
     const text = await file.text();
-
-    return c.text(`File: ${fileName}\n\n${text}`);
+    return text;
 }
+
