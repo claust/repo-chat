5fd1e3d5b980e750f996f4d7fe86527d9395c5d4
Claus Thomasen <clth@ski.dk>
2024-11-24 12:43:17 +0100

    Structure, ignore some folders

--- packages/server/updater.ts
+++ packages/server/updater.ts
@@ -4,16 +4,33 @@ import type { BlankEnv, BlankInput } from "hono/types";
 
 export async function updater(c: Context<BlankEnv, "/updater", BlankInput>): Promise<void | Response> {
     const includeText = c.req.query('includeText');
-    const files = await readdir('./', { recursive: true });
-    let result = '';
+    const folder = './../..';
+    var { files, log } = await getFilesToProcess(folder);
+
     for (const file of files) {
+        if (!await Bun.file(file).exists()) {
+            console.log('Skipping folder', file);
+            continue;
+        }
         const text = await handleFile(file);
-        result += `File: ${file}\n\n`;
+        log += `File: ${file}\n\n`;
         if (includeText === 'true') {
-            result += `${text}\n\n`;
+            log += `${text}\n\n`;
         }
     }
-    return c.text(result);
+
+    return c.text(log);
+}
+
+async function getFilesToProcess(folder: string) {
+    const filesAndDirs = await readdir(folder, { recursive: true });
+    let log = '';
+    console.log('Found', filesAndDirs.length, 'folders/files');
+    log += `Found ${filesAndDirs.length} folders/files\n\n`;
+    const files = filesAndDirs.filter(f => !f.includes('node_modules') && !f.startsWith('.git')).map(f => `${folder}/${f}`);
+    console.log('Found', files.length, 'files (excluding ignored folders)');
+    log += `Found ${files.length} files (excluding ignored folders)\n\n`;
+    return { files, log };
 }
 
 async function handleFile(fileName: string) {
