e2bdce1c3882dbb2b772a9b23df724df17c73ca7
Claus Thomasen <clth@ski.dk>
2024-11-27 22:06:22 +0100

    Adds duckduckgo search tool

--- packages/server/server/__init__.py
+++ packages/server/server/__init__.py
@@ -1,44 +1,73 @@
+from langchain_openai import ChatOpenAI
+from langchain_community.tools import DuckDuckGoSearchRun
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from typing_extensions import TypedDict
+from typing import Annotated
 import getpass
 import os
 
+
 def _set_env(var: str):
-  if not os.environ.get(var):
-    os.environ[var] = getpass.getpass(f"{var}: ")
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
 
 _set_env("OPENAI_API_KEY")
 
-from typing import Annotated
-from typing_extensions import TypedDict
-
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
 
 class State(TypedDict):
-  messages: Annotated[list, add_messages]
+    messages: Annotated[list, add_messages]
 
 
-graph_builder = StateGraph(State)
+# Tools
 
-from langchain_openai import ChatOpenAI
+searchTool = DuckDuckGoSearchRun()
+# result = searchTool.invoke("Ballerupcentrets åbningstider?")
+tools = [searchTool]
+tool_node = ToolNode(tools=tools)
+
+# LLM
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
+llm_with_tools = llm.bind_tools(tools)
+
 
 def chatbot(state: State):
-  return {"messages": llm.invoke(state["messages"])}
+    return {"messages": llm_with_tools.invoke(state["messages"])}
+
+
+# Graph
+graph_builder = StateGraph(State)
 
 # Add nodes
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
 
 # Add edges
-graph_builder.add_edge(START, "chatbot")
+# graph_builder.add_edge(START, "chatbot")
+graph_builder.set_entry_point("chatbot")
+graph_builder.add_conditional_edges(
+  "chatbot",
+  tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge("chatbot", END)
 
 graph = graph_builder.compile()
 
+
 def stream_graph_updates(user_input: str):
   for event in graph.stream({"messages": [("user", user_input)]}):
     for value in event.values():
-      print("Assistant:", value["messages"][-1].content)
+      messages = value["messages"]
+      if isinstance(messages, list):
+        message = messages[-1]
+      else:
+        message = messages
+      print("Assistant:", message.content)
+
 
 while True:
   try:
@@ -53,5 +82,3 @@ while True:
     print("User:", user_input)
     stream_graph_updates(user_input)
     break
-
-
